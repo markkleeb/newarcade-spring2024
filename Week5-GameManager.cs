82eb827270ac697b9e428e1102d4bf6d8c3aead1
Mark Kleback <kleeb930@gmail.com>
2024-03-01 10:31:52 -0500

    killed() function

--- Week5-GameManager.cs
+++ Week5-GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -11,14 +13,25 @@ public class GameManager : MonoBehaviour
     //Grab the invader object
     public GameObject invader;
 
+    //UI for scorekeeping
     public int score = 0;
     public TMP_Text scoreText;
+    private int highScore;
+
+    //variables for tracking lives
+    private int lives = 3;
+    public GameObject life;
+    public bool isDead = false;
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+
+        highScore = PlayerPrefs.GetInt("HighScore");
+
         Instantiate(ship, new Vector2(0, -3), Quaternion.identity);
 
         //draw 10 invaders across
@@ -29,7 +42,12 @@ public class GameManager : MonoBehaviour
             {
                 Instantiate(invader, new Vector2(i, j), Quaternion.identity);
             }
+        }
 
+        //Draw 3 Life Sprites
+        for (int i=0; i< 3; i++)
+        {
+            Instantiate(life, new Vector2(i + 6, 4), Quaternion.identity);
         }
 
     }
@@ -37,7 +55,12 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(isDead == true && Input.anyKeyDown)
+        {
+            Instantiate(ship, new Vector2(0, -3), Quaternion.identity);
+            isDead = false;
+
+        }
     }
 
 
@@ -45,5 +68,25 @@ public class GameManager : MonoBehaviour
     {
         score += 1;
         scoreText.text = "Score: " + score;
+
+        if(score > highScore)
+        {
+            highScore = score;
+        }
+
+    }
+
+    public void killed()
+    {
+        isDead = true;
+        lives -= 1;
+        GameObject[] lifeSprites = GameObject.FindGameObjectsWithTag("Life");
+        Destroy(lifeSprites[0]);
+
+        if (lives < 0)
+        {
+            PlayerPrefs.SetInt("HighScore", highScore);
+            SceneManager.LoadScene("TitleScene");
+        }
     }
 }
